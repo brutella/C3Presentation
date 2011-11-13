940c84f78de4443ea0433a55252ba6c723ef7579
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2011-11-13 19:17:42 +0100

    finished classes slides

--- _classes/classes.cpp
+++ _classes/classes.cpp
@@ -8,6 +8,16 @@ public:
 	string brand;
 	string constructionYear;
 	
+	int weightInKg() const{
+		// Would cause compiler error
+		// weight = 100; 
+		return weight;
+	}
+	
+	void setWeight(int _weight){
+		this->weight = _weight;
+	}
+	
 	string description(){
 		return brand + " " + constructionYear;
 	}
@@ -19,5 +29,11 @@ int main(){
 	bmw.weight = 2000;
 	bmw.brand = "bmw";
 	bmw.constructionYear = "2011";
-	cout << bmw.description();
+	cout << bmw.description() << endl;
+	
+	// try const
+	Car audi;
+	//audi.weight = 100; // error
+	audi.setWeight(100); // error
+	cout << audi.weight << endl;
 }
